219fd7ddcf73efec58042413544015b6cc98a1d3
Nenad Đapić <ndjapic@users.noreply.github.com>
2024-05-24 11:05:47 +0200

    Create k2Cv2.py

---
+++ takprog/os6-202324/k2Cv2.py
@@ -0,0 +1,16 @@
+n = int(input())
+k = int(input())
+
+if n < 5:
+    karta = 0
+if 5 <= n <= 12:
+    karta = 2800
+if 13 <= n <= 64:
+    karta = 3500
+if n >= 64:
+    karta = 3200
+
+platiti = k * karta
+if k > 10:
+    platiti -= (k-10) * karta // 2
+print(platiti)
